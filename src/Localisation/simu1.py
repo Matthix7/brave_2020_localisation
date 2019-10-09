66010019092ee201e4504c8e2e2fcbbc5536f310
Matthix7 <catamatth@gmail.com>
2019-10-09 21:08:09 +0200

    Completed simulations considering perfect conditions

--- src/Localisation/simu1.py
+++ src/Localisation/simu1.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-###################################
+############################################################
 # Localisation simulation1: 
 #     mobile: tank style
 #     landmarks: 1
@@ -9,7 +9,11 @@
 #         speed of the boat
 #     noise: no
 #     limited range: no
-###################################     
+# Remarks:
+#    works while the derivative of bearing is high enough
+############################################################  
+
+##########################  IMPORTS   ############################
 import rospy
 
 import sys
@@ -19,6 +23,9 @@ pkg = rospack.get_path('brave_2020_localisation')
 sys.path.append(pkg+'/src/Localisation')
 from roblib import *
 
+
+
+#####################  SIMULATION SETUP   ########################
 # Time step
 dt = 0.7
 
@@ -26,7 +33,9 @@ dt = 0.7
 landmark = array([[15], [15]])
 
 # Initial state of the boat
-Xinit = array([[6], [5], [0], [1]])
+Xinit = array([[6], [0], [pi/2+0.5], [1]])
+
+
 
 ##########################  FUNCTIONS   ############################
 
@@ -46,14 +55,14 @@ def getBearing(X, landmark):
     return sawtooth(atan2(yl-y, xl-x) - X[2,0])
 
 
-def approxPos(speed, heading, dHeading, bearing, dBearing):
+def approxPos1(speed, heading, dHeading, bearing, dBearing, landmark):
     # Case with one landmark, a compass and speed known (Mobile Robotics, L.Jaulin, p.131-132)
     A = array([[sin(heading+bearing), cos(heading+bearing)],
                [-cos(heading+bearing), sin(heading+bearing)]])
     
-    B = array([[-landmark[1], landmark[0]],
-               [landmark[0] + speed*sin(heading)/(dHeading+dBearing), landmark[1] - v*cos(heading)/(dHeading+dBearing)]])
-    
+    B = array([[-landmark[1, 0], landmark[0, 0]],
+               [landmark[0, 0] + speed*sin(heading)/(dHeading+dBearing), landmark[1, 0] - speed*cos(heading)/(dHeading+dBearing)]])
+#    print(B)
     C = array([[cos(heading+bearing)], [sin(heading+bearing)]])
     
     if det(B) != 0:
@@ -73,10 +82,9 @@ ax = init_figure(0, 20, 0, 20)
 
 # Initialising variables
 X = Xinit
-dHeading, dBearing = 0, 0
+headingPrev, bearingPrev = 0, 0
 
 # Main loop
-
 if __name__ == "__main__":
     
     for t in arange(0, 15, dt):
@@ -89,8 +97,17 @@ if __name__ == "__main__":
         plot([X[0], landmark[0]], [X[1], landmark[1]], color = 'green')
         text(X[0]+10*cos(X[2]+bearing)/2, X[1]+10*sin(X[2]+bearing)/2, "Bearing = "+str(bearing))
         
+        
+        # localisation
+        dBearing = (bearing - bearingPrev)/dt
+        dHeading = (X[2,0] - headingPrev)/dt  #to be replaced by IMU
+        bearingPrev, headingPrev = bearing, X[2,0]
+        Xhat = approxPos1(X[3,0], X[2,0], dHeading, bearing, dBearing, landmark)
+        if Xhat is not None:
+            draw_disk(Xhat,2,ax,'red')
+        
         # command
-        u = array([[0], [0.2]])
+        u = array([[0], [-0.15]])
         
         # evolution
         dX = evolX(X, u)        
