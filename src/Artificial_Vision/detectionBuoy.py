745b982c33d5b53ab7aae1957a1cac4671e28bb6
Matthix7 <catamatth@gmail.com>
2020-02-10 15:50:44 +0100

    Adapted marks detection node to ROS image topics.

--- src/Artificial_Vision/detectionBuoy.py
+++ src/Artificial_Vision/detectionBuoy.py
@@ -9,76 +9,9 @@ import numpy as np
 from numpy import pi, cos, sin, array, shape
 
 from std_msgs.msg import String
-
-from webcamThread import WebVideoStream
-
-
-def run():
-    rospy.init_node('image_visualisation', anonymous=True)
-
-    rate = rospy.Rate(10)
-
-    vs = WebVideoStream(resolution=(320, 240), record = False).start()
-
-    time.sleep(2)
-
-    dataCam = vs.getScaleFactor()
-
-    cv2.namedWindow("Webcam", cv2.WINDOW_NORMAL)
-
-    pub_bearings = rospy.Publisher('buoys_directions', String, queue_size = 2)
-
-    while not rospy.is_shutdown():
-        image = vs.read()
-        if image is not None:
-            bearings, image = detectBuoy(image, 0, dataCam)
-
-            buoys_bearings = String(data = str(bearings))
-            pub_bearings.publish(buoys_bearings)
-
-            #show the frame
-            cv2.imshow('Webcam',image)
-
-        key = cv2.waitKey(1) & 0xFF
-        if key == 27 or key == ord('q'):
-            break
-
-        if key == 32:
-            key = cv2.waitKey(1) & 0xFF
-            while key != 32:
-                key = cv2.waitKey(1) & 0xFF
-
-        elif key == ord('c'):
-            cv2.imwrite('sample.png',image)
-            print("Picture saved")
-
-        rate.sleep()
-
-    vs.stop()
-    cv2.destroyAllWindows()
-    
-
-
-
-
-
-def getColorRange():
-    # define range of buoy color in HSV
-    # voir https://www.google.com/search?client=firefox-b&q=%23D9E80F
-
-    hue_min = 345
-    hue_max = 400
-    sat_min = 45
-    sat_max = 100
-    val_min = 40
-    val_max = 100
-
-    lower = np.array([int(hue_min/2),int(sat_min*255/100),int(val_min*255/100)])
-    upper = np.array([int(hue_max/2),int(sat_max*255/100),int(val_max*255/100)])
-
-    return (lower, upper)
-
-
+from geometry_msgs.msg import Vector3
+from sensor_msgs.msg import Image, CameraInfo
+from cv_bridge import CvBridge, CvBridgeError
 
 
 def detectBuoy(image, roll, dataCam):
@@ -122,6 +55,74 @@ def detectBuoy(image, roll, dataCam):
 
     return bearings[:1], image     ## WHILE DETECTION NOT RELIABLE
     # return bearings, image
+
+
+
+
+def getColorRange():
+    # define range of buoy color in HSV
+    # voir https://www.google.com/search?client=firefox-b&q=%23D9E80F
+
+    hue_min = 345
+    hue_max = 400
+    sat_min = 45
+    sat_max = 100
+    val_min = 40
+    val_max = 100
+
+    lower = np.array([int(hue_min/2),int(sat_min*255/100),int(val_min*255/100)])
+    upper = np.array([int(hue_max/2),int(sat_max*255/100),int(val_max*255/100)])
+
+    return (lower, upper)
+
+
+
+
+def image_callback(data):
+    global image, bridge
+    try:
+        image = bridge.imgmsg_to_cv2(data, "bgr8")
+    except:
+        pass
+
+
+def image_info_callback(data):
+    global dataCam
+    dataCam = (data.z, (data.x, data.y))
+
+
+def run():
+    global image, bridge, dataCam
+
+    rospy.init_node('image_visualisation', anonymous=True)
+
+    rate = rospy.Rate(5)
+
+    # cv2.namedWindow("Webcam", cv2.WINDOW_NORMAL)
+
+    rospy.Subscriber("camera/image_calibrated", Image, image_callback)
+    rospy.Subscriber("camera/camera_info", Vector3, image_info_callback)
+    pub_bearings = rospy.Publisher('buoys_directions', String, queue_size = 2)
+
+    image, dataCam = None, None
+    bridge = CvBridge()
+
+    while (image is None or dataCam is None) and not rospy.is_shutdown():
+        rospy.sleep(1)
+
+    while not rospy.is_shutdown():
+        bearings, image = detectBuoy(image, 0, dataCam)
+
+        buoys_bearings = String(data = str(bearings))
+        pub_bearings.publish(buoys_bearings)
+
+        #show the frame
+        cv2.imshow('Webcam',image)
+        cv2.waitKey(1)
+
+        rate.sleep()
+
+    cv2.destroyAllWindows()
 
 
 
